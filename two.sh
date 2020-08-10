f8f360d9b7ccb90ea1f326316798082b6b1f80ae
Case Larsen <CTLarsen@lbl.gov>
2020-08-10 09:51:06 -0700

    two.sh added

---
+++ two.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "This is two.sh"
+ls -alR
+env
+
+exit 0
+And a bunch of stuff that isn't executed 
+
+#!/bin/sh
+
+# local configuration options
+
+# Note: modify at your own risk!  If you do/use anything in this
+# script that is not part of a stable API (relying on files to be in
+# specific places, specific tools, specific output, etc) there is a
+# possibility you will end up with a broken system after patching or
+# upgrading.  Changes are not supported unless under direction of
+# VMware support.
+
+# Note: This script will not be run when UEFI secure boot is enabled.
